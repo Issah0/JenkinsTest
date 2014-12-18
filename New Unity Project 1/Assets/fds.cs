708951129b5a358012d9ec28d4fa3fc8cdf62118
Folco <folco.durand@gmail.com>
2014-12-18 17:26:42 +0100

    init unity project

---
+++ New Unity Project 1/Assets/fds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fds : MonoBehaviour {
+	Animator tonanimator;
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("rock3");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
